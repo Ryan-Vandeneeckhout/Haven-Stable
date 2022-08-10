8bbeacbcad334655dd5b8c767c53c774f1e54261
Ryan-Vandeneeckhout <exigoan131@gmail.com>
2022-08-10 17:26:39 -0400

    border

--- src/components/havenRoutingComponents/UserSignInRouting.jsx
+++ src/components/havenRoutingComponents/UserSignInRouting.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import LandingPage from "../pages/LandingPage.jsx";
 
-const UserSignInRouting = (props) => {
+const UserSignInRouting = () => {
   window.history.replaceState(true, "Haven Home", "/");
 
   return (
@@ -10,7 +10,6 @@ const UserSignInRouting = (props) => {
         <Route extact path="/" element={<LandingPage />} />
         <Route extact path="/createuser" element={<Navigate to="/" />} />
       </Routes>
-      <button onClick={props.UserAuth}>User {props.user}</button>
     </main>
   );
 };
