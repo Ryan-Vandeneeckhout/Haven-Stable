f34937bef68c3655879095146d3040cddbfd6c9f
emir4ik-code <emirahmetov@gmail.com>
2022-08-11 18:34:33 -0400

    userSearch api fix needed

---
+++ src/components/pages/UserSearch.js
@@ -0,0 +1,19 @@
+import React from "react";
+import UserCard from "../userProfile/UserCard";
+
+const userSearch = () => {
+    const [users,setUsers] = React.useState([]);
+    try {
+        const res = await fetch("")
+        
+    } catch (error) {
+        
+    }
+  return (
+    <div>
+      <></>
+    </div>
+  );
+};
+
+export default userSearch;
