80b0d12c097a0416f3a90311946ea7a2204bfea0
Ryan-Vandeneeckhout <exigoan131@gmail.com>
2022-08-13 06:46:45 -0400

    post

--- src/components/axiosCalls/AxiosPOST.jsx
+++ src/components/axiosCalls/AxiosPOST.jsx
@@ -2,34 +2,29 @@ import axios from "axios";
 
 export const AxiosPOST = (props) => {
   const AxiosCall = () => {
-      const body = props.body;
-      console.log(body);
-      
-      axios({
-        method: "POST",
-        url: props.APICallUrl,
-        responseType: "json",
-        body: JSON.stringify(body),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      
+    const body = props.body;
+    console.log(body);
+
+    axios({
+      method: "POST",
+      url: props.APICallUrl,
+      responseType: "json",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((jsonResponse) => {
+        props.setItem(jsonResponse.data);
       })
-        .then((jsonResponse) => {
-          props.setItem(jsonResponse.data);
-        })
-        .catch((err) => {
-          //Api error handling.
-          if (err.message === "Not Found") {
-            props.setErrorAPI("Something went wrong.");
-          } else {
-            props.setErrorAPI("Please try again.");
-          }
-        });
+      .catch((err) => {
+        //Api error handling.
+        if (err.message === "Not Found") {
+          props.setErrorAPI("Something went wrong.");
+        } else {
+          props.setErrorAPI("Please try again.");
+        }
+      });
   };
-    return (
-        <>
-        {AxiosCall()}
-        </>
-    );
+  return <>{AxiosCall()}</>;
 };
