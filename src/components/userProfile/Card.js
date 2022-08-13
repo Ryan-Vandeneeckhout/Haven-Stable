70d683f9ed764db74c54fe677e0a449486ced3df
emir4ik-code <emirahmetov@gmail.com>
2022-08-12 21:02:28 -0400

    render user interests when searching

--- src/components/userProfile/Card.js
+++ src/components/userProfile/Card.js
@@ -3,7 +3,35 @@ import UserInterest from "./UserInterest";
 import "./card.css";
 
 function Card(props) {
-  console.log(props.user);
+  const [userInterests, setUserInterests] = React.useState([]);
+
+  const fetchInterests = async () => {
+    try {
+      const res = await fetch(
+        `https://haven-nodejs.herokuapp.com/users/profile/${props.user.userInfo.id}/interests`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            token: localStorage.getItem("token"),
+          },
+        }
+      );
+      const parseRes = await res.json();
+      setUserInterests(parseRes);
+      console.log(userInterests);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+React.useEffect(() => {
+  fetchInterests();
+}, [])
+  const arr = Array.from(userInterests)
+  console.log(arr)
+  const interests = arr.map((interest) => {
+    return <UserInterest interests={interest.interest} key={interest.id} />;
+  });
   return (
     // <div>
     //   <div className="user_profile">
@@ -20,6 +48,7 @@ function Card(props) {
       <div class="card user_card">
         <img src="" className="card-img-top " alt="" />
         <div class="card-body">
+          <div>{interests}</div>
           <h4 class="card-text">{props.user.userInfo.username}</h4>
           <h5>
             {props.user.userInfo.first_name} {props.user.userInfo.last_name}
