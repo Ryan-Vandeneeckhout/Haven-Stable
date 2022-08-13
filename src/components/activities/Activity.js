ef05a9df1770482e59c32b6557a3b03c9151f721
emir4ik-code <emirahmetov@gmail.com>
2022-08-12 23:51:19 -0400

    basic user activity rendering

--- src/components/activities/Activity.js
+++ src/components/activities/Activity.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-const Activity = () => {
+const Activity = (props) => {
+    console.log(props)
   return (
     <div>
-        <h1></h1>
+        <h1>{props.props.title}</h1>
+        <h3>{props.props.content}</h3>
+        <div className = "d-flex flex-column">
+            <h4>{props.props.budget}</h4>
+            <h4>{props.props.spots_open}</h4>
+            <h4>{props.props.spots_total}</h4>
+        </div>
     </div>
   );
 };
